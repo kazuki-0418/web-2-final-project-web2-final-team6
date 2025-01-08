0b410580bd347b40cfe52a90ab62fc7479593325
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-01-08 10:15:02 -0800

    Sendgrid

--- final-project/astro.config.mjs
+++ final-project/astro.config.mjs
@@ -4,8 +4,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel()
