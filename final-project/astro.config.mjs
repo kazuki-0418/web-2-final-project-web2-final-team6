6d2c195e7f1a9c5dd9a6bde49f2b3e43a7d8de1f
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-01-08 10:30:49 -0800

    fix routing & false devtoolBar

--- final-project/astro.config.mjs
+++ final-project/astro.config.mjs
@@ -6,5 +6,8 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  devToolbar: {
+    enabled: false,
+  },
 });
